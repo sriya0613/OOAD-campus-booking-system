4e3fc8707fd1dbaee8eadc7f93ac33d2d1add4d7
sriya <sriyaramachandrula@gmail.com>
2026-04-21 12:17:29 +0530

    Update UserController.java

--- src/main/java/com/campus/booking/controller/UserController.java
+++ src/main/java/com/campus/booking/controller/UserController.java
@@ -5,6 +5,8 @@ import com.campus.booking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,7 +22,18 @@ public class UserController {
 
     // GET ALL USERS
     @GetMapping
-    public java.util.List<User> getAllUsers() {
+    public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    // LOGIN API
+    @PostMapping("/login")
+    public User login(@RequestBody User loginUser) {
+
+        return userRepository.findAll().stream()
+                .filter(u -> u.getName().equals(loginUser.getName())
+                        && u.getPassword().equals(loginUser.getPassword()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Invalid credentials"));
     }
 }
